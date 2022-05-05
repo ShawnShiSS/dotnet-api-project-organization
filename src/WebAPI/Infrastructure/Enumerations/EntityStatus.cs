939854e1e4b3ac525927a44571dbcbbd9b403e8a
Shawn Shi <ss.shawnshi@gmail.com>
2022-05-05 09:37:46 -0400

    Enums

---
+++ src/WebAPI/Infrastructure/Enumerations/EntityStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAPI.Infrastructure.Enumerations
+{
+    /// <summary>
+    ///     Entity status for soft-deleting.
+    /// </summary>
+    public enum EntityStatus
+    {
+        Active = 1,
+        Deleted = 2
+    }
+}
