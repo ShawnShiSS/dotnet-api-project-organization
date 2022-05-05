82073dccd884cecc1f5b228881c89e6e782c669c
Shawn Shi <ss.shawnshi@gmail.com>
2022-05-05 09:37:52 -0400

    Exceptions

---
+++ src/WebAPI/Infrastructure/Exceptions/ResourceNotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WebAPI.Infrastructure.Exceptions
+{
+    public class ResourceNotFoundException : Exception
+    {
+        public ResourceNotFoundException() { }
+        public ResourceNotFoundException(string message) : base(message) { }
+        public ResourceNotFoundException(string message, Exception innerException) : base(message, innerException)
+        { }
+
+        public ResourceNotFoundException(string name, object key)
+            : base($"Resource \"{name}\" ({key}) was not found.")
+        {
+        }
+    }
+}
