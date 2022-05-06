8fc577cabbabf0aa0ab5f2b7d70085439e65b1c5
Shawn Shi <ss.shawnshi@gmail.com>
2022-05-06 06:05:50 -0400

    enum

---
+++ src/WebAPI/Infrastructure/Enumerations/ForecastRange.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAPI.Infrastructure.Enumerations
+{
+    /// <summary>
+    ///     Forecast time ranges.
+    /// </summary>
+    public enum ForecastRange
+    {
+        OneDay = 1,
+        SevenDay = 2
+    }
+}
